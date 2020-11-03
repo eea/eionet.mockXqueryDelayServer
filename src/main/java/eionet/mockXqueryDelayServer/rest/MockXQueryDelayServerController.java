b8f57285ec4f94b1c87dc0187eb8f14689c7e70d
vskiadas90 <vskiadas90@gmail.com>
2020-11-03 14:49:16 +0200

    - additional endpoint to mimic delayed response from Content Registry Sparql endpoint

--- src/main/java/eionet/mockXqueryDelayServer/rest/MockXQueryDelayServerController.java
+++ src/main/java/eionet/mockXqueryDelayServer/rest/MockXQueryDelayServerController.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @RestController
 @RequestMapping({ "","/api"})
@@ -19,15 +22,28 @@ public class MockXQueryDelayServerController {
     Long waitingTime;
     Logger logger = LoggerFactory.getLogger(MockXQueryDelayServerController.class);
 
-    @GetMapping({ "/test","/sparql"})
-    public String test(@RequestParam(value="query" ,required = false)String query, HttpServletRequest request) throws InterruptedException {
-        if(query!=null){
-            logger.info("Query is:"+query);
-        }
-        String remoteHost = request.getRemoteHost();
-        if (remoteHost.contains("eionet.europa.eu")) {
+    @GetMapping("/test")
+    public String test( HttpServletRequest request) throws InterruptedException {
             Thread.sleep(waitingTime);
-        }
         return "";
+    }
+
+
+    @GetMapping("/sparql")
+    public StreamingResponseBody mimicCrSparqlResultDownload(@RequestParam(value="query" ,required = false)String query, HttpServletRequest request) throws InterruptedException {
+            logger.info("Query is:"+query);
+
+        return out -> {
+            for (int i = 0; i < 100; i++) {
+                out.write((i + " - demo demo demo \n ")
+                        .getBytes());
+                out.flush();
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 }

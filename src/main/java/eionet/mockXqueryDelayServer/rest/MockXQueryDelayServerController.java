2fac95cf4c6921d173d27ac4fae01ea67ca32575
vskiadas90 <vskiadas90@gmail.com>
2020-11-24 13:35:22 +0200

    - added endpoint to verify application works through browser

--- src/main/java/eionet/mockXqueryDelayServer/rest/MockXQueryDelayServerController.java
+++ src/main/java/eionet/mockXqueryDelayServer/rest/MockXQueryDelayServerController.java
@@ -3,10 +3,7 @@ package eionet.mockXqueryDelayServer.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +18,13 @@ public class MockXQueryDelayServerController {
     @Value("${waitingTime}")
     Long waitingTime;
     Logger logger = LoggerFactory.getLogger(MockXQueryDelayServerController.class);
+
+
+    @GetMapping("/")
+    @ResponseBody
+    public String itWorks() throws InterruptedException {
+        return "<h1>It works!<h1>";
+    }
 
     @GetMapping("/test")
     public String test( HttpServletRequest request) throws InterruptedException {
